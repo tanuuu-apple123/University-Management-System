b5b6723f53a8251177df5162c06cb257c1bf614b
Akunuri Tanavya <akunuritanavya@gmail.com>
2025-12-29 20:38:36 +0530

    Update Connec.java

--- src/university/management/system/Connec.java
+++ src/university/management/system/Connec.java
@@ -7,7 +7,7 @@ public class Connec {
     Connec(){
         try{
            Class.forName("com.mysql.cj.jdbc.Driver"); //register driver class
-           c=DriverManager.getConnection("jdbc:mysql://localhost:3306/universitymanagementsystem","root","Durin50@50@1");
+           c=DriverManager.getConnection("jdbc:mysql://localhost:3306/universitymanagementsystem","root","Password");
            s=c.createStatement();//to execute mysql statements
         }catch(Exception e){   //if port number is3306 can be removed//databasename,username,password
             e.printStackTrace();
